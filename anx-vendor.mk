388c9197f29958d72d2599b7f1a5f7e4f10774da
sarveshrulz <sarveshkardekar@gmail.com>
2020-06-05 22:56:34 +0530

    ANXCamera: update to v183 EasygoingForthrightEasternnewt

--- anx-vendor.mk
+++ anx-vendor.mk
@@ -5,10 +5,12 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/product/bin,$(TARGET_COPY_OUT_PRODUCT)/bin) \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/product/etc,$(TARGET_COPY_OUT_PRODUCT)/etc) \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/product/priv-app/ANXCamera/lib,$(TARGET_COPY_OUT_PRODUCT)/priv-app/ANXCamera/lib) \
+    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/product/priv-app/ANXExtraPhoto/lib,$(TARGET_COPY_OUT_PRODUCT)/priv-app/ANXExtraPhoto/lib) \
     $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc)
 
 PRODUCT_PACKAGES += \
-    ANXCamera
+    ANXCamera \
+    ANXExtraPhoto
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.miui.notch=1
