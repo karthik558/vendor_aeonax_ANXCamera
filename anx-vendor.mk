200bd830938ef78dae1af28253fb66039561b2d4
sarveshrulz <sarveshkardekar@gmail.com>
2020-08-06 10:42:11 +0530

    ANXCamera: update to v190 BountifulDistantHapuka & move everything back to /system

--- anx-vendor.mk
+++ anx-vendor.mk
@@ -2,15 +2,19 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/aeonax/ANXCamera
 
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/product/bin,$(TARGET_COPY_OUT_PRODUCT)/bin) \
-    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/product/etc,$(TARGET_COPY_OUT_PRODUCT)/etc) \
-    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/product/priv-app/ANXCamera/lib,$(TARGET_COPY_OUT_PRODUCT)/priv-app/ANXCamera/lib) \
-    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/product/priv-app/ANXExtraPhoto/lib,$(TARGET_COPY_OUT_PRODUCT)/priv-app/ANXExtraPhoto/lib) \
-    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc)
+    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/bin,$(TARGET_COPY_OUT_SYSTEM)/bin) \
+    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
+    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/framework,$(TARGET_COPY_OUT_SYSTEM)/framework) \
+    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/lib,$(TARGET_COPY_OUT_SYSTEM)/lib) \
+    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
+    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/system/priv-app/ANXScanner/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/ANXScanner/lib) \
+    $(call find-copy-subdir-files,*,vendor/aeonax/ANXCamera/proprietary/vendor/lib64,$(TARGET_COPY_OUT_VENDOR)/lib64)
 
 PRODUCT_PACKAGES += \
     ANXCamera \
-    ANXExtraPhoto
+    ANXExtraPhoto \
+    ANXScanner \
+    anxres
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.miui.notch=1
